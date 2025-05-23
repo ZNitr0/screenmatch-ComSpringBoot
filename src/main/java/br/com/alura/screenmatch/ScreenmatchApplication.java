8a96db333efc95f8c60ad7d45870a0eb044fd01e
ZNitr0 <levidonascimento2410@gmail.com>
2025-05-23 11:13:03 -0300

    Lista de Temporadas

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,15 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverterDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -22,5 +26,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConverterDados conversor = new ConverterDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+		for(int i = 1; i<=dados.totalTemporadas(); i++){
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=bd4571b8");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
