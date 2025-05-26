06b7310b7b717bb81f2c5af0816463fedb13d429
ZNitr0 <levidonascimento2410@gmail.com>
2025-05-26 19:54:51 -0300

    criação da classe episodio para uma melhor descrição.

---
+++ src/main/java/br/com/alura/screenmatch/model/Episodio.java
@@ -0,0 +1,78 @@
+package br.com.alura.screenmatch.model;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer numeroEpisodio;
+    private double avaliacao;
+    private LocalDate dataEpisodio;
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
+        this.temporada = numeroTemporada;
+        this.titulo = dadosEpisodio.titulo();
+        this.numeroEpisodio = dadosEpisodio.numero();
+        try {
+            this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
+        } catch (NumberFormatException ex) {
+            this.avaliacao = 0.0;
+        }
+        try {
+            this.dataEpisodio = LocalDate.parse(dadosEpisodio.dataLancamento());
+        } catch (DateTimeException ex) {
+            this.dataEpisodio = null;
+        }
+    }
+
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataEpisodio() {
+        return dataEpisodio;
+    }
+
+    public void setDataEpisodio(LocalDate dataEpisodio) {
+        this.dataEpisodio = dataEpisodio;
+    }
+
+    @Override
+    public String toString() {
+        return "temporada=" + temporada +
+                ", titulo='" + titulo + '\'' +
+                ", numeroEpisodio=" + numeroEpisodio +
+                ", avaliacao=" + avaliacao +
+                ", dataEpisodio=" + dataEpisodio ;
+    }
+}
